5f4f3977397172ae02ad627ed6076915707ec7d6
Brian <bybyby@gmail.com>
2021-03-24 19:57:26 -0700

    Add onChange and onClick handlers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const[name, setName] = useState("");
+  const[enteredName, setEnteredName] = useState("");
+
+  function handleClick() {
+    console.log(enteredName);
+    setEnteredName(name)
+
+  }
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {enteredName}</h1>
+      <input 
+        onChange={handleChange}
+        type="text" 
+        placeholder="What's your name?" 
+        value={name}  
+        />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
